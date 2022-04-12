a6143d064dfec8785ba530bf501f7f09b31db129
ysh717 <tmd5675@naver.com>
2022-04-12 16:23:33 +0900

    0412_유효성검사

--- src/main/java/com/mycompany/webapp/controller/Ch04Controller.java
+++ src/main/java/com/mycompany/webapp/controller/Ch04Controller.java
@@ -1,11 +1,19 @@
 package com.mycompany.webapp.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.mycompany.webapp.dto.Ch04Dto;
 import com.mycompany.webapp.dto.Ch04Member;
+import com.mycompany.webapp.validator.Ch04MemberJoinFormValidator;
+import com.mycompany.webapp.validator.Ch04MemberLoginFormValidator;
 
 import lombok.extern.log4j.Log4j2;
 
@@ -25,10 +33,49 @@ public class Ch04Controller {
 		return "ch04/content";
 	}
 	
+	//DTO와 유효성 검사기를 연결
+	@InitBinder("joinForm")
+	public void bindCh04MemberJoinFormValidator(WebDataBinder binder) {
+		binder.setValidator(new Ch04MemberJoinFormValidator());
+	}
+	
 	@PostMapping("/join")
-	public String join(Ch04Member member) {
-		log.info(member);
+	public String join(@ModelAttribute("joinForm") @Valid Ch04Member member, Errors errors) {
+		log.info("실행");
+		
+		//유횽성 검사 확인
+		if(errors.hasErrors()) {
+			//다시 입력 폼으로 돌아가기
+			return "ch04/content";
+		}
+		
 		//회원 가입 처리
+		//...
+		
+		//홈페이지로 이동
 		return "redirect:/";
 	}
+	
+		//DTO와 유효성 검사기를 연결
+		@InitBinder("loginForm")
+		public void bindCh04MemberLoginFormValidator(WebDataBinder binder) {
+			binder.setValidator(new Ch04MemberLoginFormValidator());
+		}
+		
+		@PostMapping("/login")
+		public String login(@ModelAttribute("loginForm") @Valid Ch04Member member, Errors errors) {
+			log.info("실행");
+			
+			//유횽성 검사 확인
+			if(errors.hasErrors()) {
+				//다시 입력 폼으로 돌아가기
+				return "ch04/content";
+			}
+			
+			//회원 가입 처리
+			//...
+			
+			//홈페이지로 이동
+			return "redirect:/";
+		}
 }
--- src/main/java/com/mycompany/webapp/validator/Ch04MemberJoinFormValidator.java
+++ src/main/java/com/mycompany/webapp/validator/Ch04MemberJoinFormValidator.java
@@ -1,5 +1,8 @@
 package com.mycompany.webapp.validator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
@@ -15,12 +18,56 @@ public class Ch04MemberJoinFormValidator implements Validator {
 	public boolean supports(Class<?> clazz) {
 		log.info("실행");
 		boolean result = Ch04Member.class.isAssignableFrom(clazz);
+		log.info(result);
 		return result;
 	}
 
 	@Override
 	public void validate(Object target, Errors errors) {
+		log.info("실행");
+		Ch04Member member = (Ch04Member)target;
 		
+		//mid 검사
+		if(member.getMid() == null || member.getMid().trim().equals("")) {
+			errors.rejectValue("mid", "errors.mid.required","mid는 필수 입력 사항입니다.");
+		} else {
+			if(member.getMid().length() < 4) {
+				errors.rejectValue("mid", "errors.mid.minlength", new Object[] {4}, "mid는 최소 4자 이상입니다.");
+			}
+		}
+		
+		//mpassword 검사
+		if(member.getMpassword() == null || member.getMpassword().trim().equals("")) {
+			errors.rejectValue("mpassword", "errors.mpassword.required","mpassword는 필수 입력 사항입니다.");
+		} else {
+			if(member.getMid().length() < 8) {
+				errors.rejectValue("mpassword", "errors.mpassword.minlength", new Object[] {8}, "mpassword는 최소 8자 이상입니다.");
+			}
+		}
+		
+		//memail 검사
+		if(member.getMemail() == null || member.getMemail().trim().equals("")) {
+			errors.rejectValue("memail", "errors.memail.required","memail는 필수 입력 사항입니다.");
+		} else {
+			String regex = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+			Pattern pattern = Pattern.compile(regex);
+			Matcher matcher = pattern.matcher(member.getMemail());
+			if(!matcher.matches()) {
+				errors.rejectValue("memail", "errors.memail.invalid", "memail은 이메일 형식이 아닙니다.");
+			}
+		}
+		
+		//mtel 검사
+		if(member.getMtel() == null || member.getMtel().trim().equals("")) {
+			errors.rejectValue("mtel", "errors.mtel.required", "mtel은 필수 입력 사항입니다.");
+		} else {
+			String regex = "^\\d{3}-\\d{3,4}-\\d{4}$";
+			Pattern pattern = Pattern.compile(regex);
+			Matcher matcher = pattern.matcher(member.getMtel());
+			if(!matcher.matches()) {
+				errors.rejectValue("mtel", "errors.mtel.invalid", "mtel은 전화번호 형식이 압니다.");
+			}
+		}
 	}
 
 }
