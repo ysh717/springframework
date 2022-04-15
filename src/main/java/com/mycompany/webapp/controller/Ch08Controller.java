dfe1e9f3d2c6e4488492e3d908cdd38967f517d8
ysh717 <tmd5675@naver.com>
2022-04-15 11:56:04 +0900

    0415_Ch08 Session Support

--- src/main/java/com/mycompany/webapp/controller/Ch08Controller.java
+++ src/main/java/com/mycompany/webapp/controller/Ch08Controller.java
@@ -1,20 +1,27 @@
 package com.mycompany.webapp.controller;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+
+import com.mycompany.webapp.dto.Ch08InputForm;
 
 import lombok.extern.log4j.Log4j2;
 
 @Controller
 @RequestMapping("/ch08")
 @Log4j2
+@SessionAttributes({"inputForm"})
 public class Ch08Controller {
 	@RequestMapping("/content")
 	public String content() {
@@ -85,4 +92,71 @@ public class Ch08Controller {
 		log.info("mid:" + mid);
 		return "redirect:/ch08/content";
 	}
+	
+	@RequestMapping(value = "/loginAjax", produces = {"application/json; charset = UTF-8"})
+	@ResponseBody
+	public String loginAjax(String mid, String mpassword, HttpSession session) {
+		String result = null;
+		if(mid.equals("spring")) {
+			if(mpassword.equals("12345")) {
+				result = "success";
+				session.setAttribute("sessionMid", mid);
+			} else {
+				result = "wrongMpassword";
+			}
+		} else {
+			result = "wrongMid";
+		}
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("result", result);
+		String json = jsonObject.toString();
+		return json;
+	}
+	
+	@RequestMapping(value = "/logoutAjax", produces = {"application/json; charset = UTF-8"})
+	@ResponseBody
+	public String logoutAjax(HttpSession session, HttpServletRequest request) {
+		//세션에서 주어진 키를 삭제
+		session.removeAttribute("sessionMid");
+		//세션 객체 자체를 제거
+		/*request.getSession().invalidate();
+		request.getSession(true);*/
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("result", "success");
+		String json = jsonObject.toString();
+		return json;
+	}
+	
+	//새로운 세션 저장소에 객체를 저장하는 역할, 단 한번만 실행
+	@ModelAttribute("inputForm")
+	public Ch08InputForm getCh08InputForm() {
+		log.info("실행");
+		Ch08InputForm inputForm = new Ch08InputForm();
+		return inputForm;
+	}
+	
+	@GetMapping("/inputStep1")
+	public String inputStep1() {
+		
+		return "ch08/inputStep1";
+	}
+	
+	@PostMapping("/inputStep2")
+	public String inputStep2(@ModelAttribute("inputForm") Ch08InputForm inputForm) {
+		log.info("data1 : "+ inputForm.getData1());
+		log.info("data2 : "+ inputForm.getData2());
+		return "ch08/inputStep2";
+	}
+	
+	@PostMapping("/inputDone")
+	public String inputStep3(@ModelAttribute("inputForm") Ch08InputForm inputForm, SessionStatus sessionStatus) {
+		log.info("data1 : "+ inputForm.getData1());
+		log.info("data2 : "+ inputForm.getData2());
+		log.info("data3 : "+ inputForm.getData3());
+		log.info("data4 : "+ inputForm.getData4());
+		sessionStatus.setComplete();
+		return "redirect:/ch08/content";
+	}
+	
+	
 }
